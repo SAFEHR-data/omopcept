6eadcefa86433a9bdd955751d2655ff77f38df43
AndySouth <southandy@gmail.com>
2023-04-26 12:25:48 +0100

    use_r("utils") and add pipe_if()

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+
+#' conditionally apply a pipe
+#'
+#' @param df dataframe to apply funct to if cond TRUE
+#' @param cond condition whether to run func, not vectorised (should be a single logical)
+#' @param func function to run
+#' @example data.frame(a=c(1:5)) |> pipe_if(TRUE, \(d) d |> mutate(b=a*2))
+pipe_if <- function(df, cond, func) {
+  if (cond) func(df)
+  else df
+}
