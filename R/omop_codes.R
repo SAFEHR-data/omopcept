f4bebc209c130799c1242b08f5fdc08435a4c05d
AndySouth <southandy@gmail.com>
2023-05-15 19:04:09 +0100

    rename to omop_codes, move to its own source file add ocodes()

---
+++ R/omop_codes.R
@@ -0,0 +1,52 @@
+#' string search of concept_code in omop concepts table
+#'
+#' @param findstring string to search for or regex
+#' @param ignore_case ignore case in string comparison, default TRUE
+# @param negate If TRUE, return non-matching elements, default FALSE
+#' @param c_ids one or more concept_id to filter by, default NULL for all
+#' @param d_ids one or more domain_id to filter by, default NULL for all
+#' @param v_ids one or more vocabulary_id to filter by, default NULL for all
+#' @param cc_ids one or more concept_class_id to filter by, default NULL for all
+#' @param standard one or more standard_concept to filter by, default NULL for all, S,C
+#' @export
+#' @examples
+#' omop_codes("AJCC/UICC-Stage")
+
+omop_codes <- function( findstring,
+                           ignore_case = TRUE,
+                           #negate = FALSE,
+                           c_ids=NULL,
+                           d_ids=NULL,
+                           v_ids=NULL,
+                           cc_ids=NULL,
+                           standard=NULL) {
+
+  df1 <- omopcepts::open_concept() |>
+
+    #TODO put negate back in if possible
+
+    filter(arrow_match_substring_regex(concept_code,
+                                       options=list(pattern=findstring,
+                                                    ignore_case=ignore_case))) |>
+    collect() |>
+
+    omop_filter_concepts(c_ids=c_ids, d_ids=d_ids, v_ids=v_ids, cc_ids=cc_ids, standard=standard)
+
+
+    #filter(grepl(findstring,.data$concept_code, ignore.case=ignore_case))
+    #Error: Filter expression not supported for Arrow Datasets: grepl(pattern, .data$concept_name, ignore_case = ignore_case)
+    #Call collect() first to pull data into R.
+    #filter(stringr::str_detect(.data$concept_code, stringr::regex(, ignore_case=ignore_case), negate=negate)) |>
+
+
+  return(df1)
+
+}
+
+
+#' super short name func to search concepts by concept_code
+#' @rdname omop_codes
+#' @export
+#' @examples
+#' ocodes("AJCC/UICC-Stage")
+ocodes <- omop_codes
--- R/omop_names.R
+++ R/omop_names.R
@@ -61,50 +61,15 @@ omop_names <- function(#df1 = NULL,
 
 }
 
-#' string search of concept_code in omop concepts table
-#'
-#' @param findstring string to search for or regex
-#' @param ignore_case ignore case in string comparison, default TRUE
-# @param negate If TRUE, return non-matching elements, default FALSE
-#' @param c_ids one or more concept_id to filter by, default NULL for all
-#' @param d_ids one or more domain_id to filter by, default NULL for all
-#' @param v_ids one or more vocabulary_id to filter by, default NULL for all
-#' @param cc_ids one or more concept_class_id to filter by, default NULL for all
-#' @param standard one or more standard_concept to filter by, default NULL for all, S,C
+
+#' super short name func to search concepts by concept_name
+#' @rdname omop_names
 #' @export
 #' @examples
-#' concept_codes("AJCC/UICC-Stage")
-
-concept_codes <- function( findstring,
-                           ignore_case = TRUE,
-                           #negate = FALSE,
-                           c_ids=NULL,
-                           d_ids=NULL,
-                           v_ids=NULL,
-                           cc_ids=NULL,
-                           standard=NULL) {
-
-  df1 <- omopcepts::open_concept() |>
-
-    #TODO put negate back in if possible
-
-    filter(arrow_match_substring_regex(concept_code,
-                                       options=list(pattern=findstring,
-                                                    ignore_case=ignore_case))) |>
-    collect() |>
-
-    omop_filter_concepts(c_ids=c_ids, d_ids=d_ids, v_ids=v_ids, cc_ids=cc_ids, standard=standard)
+#' onames("chemotherapy", v_ids="LOINC")
+onames <- omop_names
 
 
-    #filter(grepl(findstring,.data$concept_code, ignore.case=ignore_case))
-    #Error: Filter expression not supported for Arrow Datasets: grepl(pattern, .data$concept_name, ignore_case = ignore_case)
-    #Call collect() first to pull data into R.
-    #filter(stringr::str_detect(.data$concept_code, stringr::regex(, ignore_case=ignore_case), negate=negate)) |>
-
-
-  return(df1)
-
-}
 
 #ISSUE is that can't pass a string varible within a function - its not recognised by arrow
 #but can pass stringvar outside of a function
