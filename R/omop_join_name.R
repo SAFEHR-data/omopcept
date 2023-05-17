6eddc5876553803eb527878940ab0f626ee0e0dc
AndySouth <southandy@gmail.com>
2023-05-17 17:51:21 +0100

    created an OMOP correct example where columns are consistent between rows

--- R/omop_join_name.R
+++ R/omop_join_name.R
@@ -49,9 +49,8 @@ ojoin <- omop_join_name
 #' @return dataframe based on input df with 1 extra column added for each concept_id column
 #' @export
 #' @examples
-#' #TODO create an OMOP correct example where columns are consistent between rows
-#' data.frame(concept_id=(c(3571338L,4002075L)),
-#'            drug_concept_id=(c(4000794L,4002592L))) |>
+#' data.frame(route_concept_id=(c(4132161L,	4171047L)),
+#'            drug_concept_id=(c(1550560L,	35780880L))) |>
 #'            omop_join_name_all()
 #' #df2 <- drug_exposure %>%
 #' #       head(100)) %>%
