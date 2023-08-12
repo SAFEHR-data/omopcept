9ddc5121f05f7a6324dcab28d6ed8c4e72f02bfb
AndySouth <southandy@gmail.com>
2023-08-12 22:53:47 +0100

    first sensible plot, next labels

--- R/omop_graph.R
+++ R/omop_graph.R
@@ -13,7 +13,7 @@
 #' @export
 #' @examples
 #' #TODO enable example when working
-#' ggr1 <- omop_graph(v_ids="Cancer Modifier", separation=1)
+#' #ggr1 <- omop_graph(v_ids="Cancer Modifier", separation=1)
 #' #omop_graph(1633308)
 #' #omop_graph("Non-invasive blood pressure")
 #' #omop_graph("Non-invasive blood pressure",separation=c(1,2))
@@ -29,10 +29,6 @@ omop_graph <- function(c_id=NULL,
                               messages=TRUE
                            ) {
 
-  #TODO fix these errors
-  #! Failed to load R/omop_graph.R
-  #Caused by error in `check_c_id()`: ! could not find function "check_c_id"
-
   # TODO add omop_ancestors & option to do both
 
 
@@ -47,28 +43,43 @@ omop_graph <- function(c_id=NULL,
 
   #ancestor_name, concept_name, min_levels_of_separation
 
-  # ggraph requires two data frames, one for nodes and one for edges.
+  df2 <- df1 |> filter(str_detect(concept_name,"FIGO"))
 
-  nodes <- c(df1$ancestor_name, df1$concept_name) |>
-    unique() |>
-    tibble(label = .) |>
-    rowid_to_column("id")
-
-  # character names need to be node IDs.
-  # done by 2 joins to node dataframe.
-
-  edges <- df1 |>
-    left_join(nodes, by = c("ancestor_name"="label")) |>
-    rename(from = "id") |>
-    left_join(nodes, by = c("concept_name"="label")) |>
-    rename(to = "id") |>
-    select(from, to, min_levels_of_separation)
-
-  grapho <- tbl_graph(nodes = nodes, edges = edges, directed = FALSE)
+  grapho <- df2 |>
+    dplyr::select(ancestor_name,
+           concept_name,
+           vocabulary_id,
+           domain_id) |>
+    dplyr::rename(from = ancestor_name,
+                  to = concept_name) |>
+    as_tbl_graph()
 
   ggr <- ggraph(grapho) +
-    geom_edge_link(aes(colour = factor(min_levels_of_separation))) +
+    geom_edge_link() +
+    #geom_edge_link(aes(colour = factor(min_levels_of_separation))) +
     geom_node_point()
+
+  plot(ggr)
+
+  ##########################
+  #trying to get node labels
+
+  #simple example from help
+  simple <- create_notable('bull') %>%
+    mutate(name = c('Thomas', 'Bob', 'Hadley', 'Winston', 'Baptiste')) %>%
+    activate(edges) %>%
+    mutate(type = sample(c('friend', 'foe'), 5, TRUE))
+
+  ggraph(simple, layout = 'graphopt') +
+         geom_edge_link(aes(start_cap = label_rect(node1.name),
+                            end_cap = label_rect(node2.name)),
+                            arrow = arrow(length = unit(4, 'mm'))) +
+         geom_node_text(aes(label = name))
+
+  plot(ggr)
+
+
+
 
 
   #if (messages) message("returning ",nrow(df1)," concepts")
@@ -77,7 +88,7 @@ omop_graph <- function(c_id=NULL,
 
 }
 
-#' super short name func to find ancestors
+#' super short name graph func
 #' @rdname omop_graph
 #' @export
 #' @examples
@@ -85,3 +96,40 @@ omop_graph <- function(c_id=NULL,
 #' # arguments e.g. v for v_ids, cc for cc_ids
 #' disabled to try to fix error : 'omop_concept_ancestor' is not an exported object from 'namespace:omopcept'
 #omgr <- omop_graph
+
+
+# ggraph requires two data frames, one for nodes and one for edges.
+#I think that was the old version
+# nodes <- df1 |>
+#   select(ancestor_name,concept_name,vocabulary_id,domain_id) |>
+#   mutate(id=row_number())
+
+# old way
+# nodes <- c(df1$ancestor_name, df1$concept_name) |>
+#   unique() |>
+#   tibble::tibble(label = .) |>
+#   tibble::rowid_to_column("id")
+
+# character names need to be node IDs.
+# done by 2 joins to node dataframe.
+
+# edges <- df1 |>
+#   left_join(nodes, by = c("ancestor_name"="ancestor_name")) |>
+#   rename(from = "id") |>
+#   left_join(nodes, by = c("concept_name"="concept_name")) |>
+#   rename(to = "id") |>
+#   select(from, to, min_levels_of_separation,vocabulary_id,domain_id)
+
+# old way
+# edges <- df1 |>
+#   left_join(nodes, by = c("ancestor_name"="label")) |>
+#   rename(from = "id") |>
+#   left_join(nodes, by = c("concept_name"="label")) |>
+#   rename(to = "id") |>
+#   select(from, to, min_levels_of_separation)
+
+# grapho <- tbl_graph(nodes = nodes, edges = edges, directed = FALSE)
+#
+# nodes <- df1 |>
+#   select(ancestor_name,concept_name,vocabulary_id,domain_id) |>
+#   mutate(id=row_number())
