8e25032df508a1d0f62b4737069a6601e96f3803
AndySouth <southandy@gmail.com>
2023-10-19 19:17:30 +0100

    #DEVNOTE from NY first go at dev of multiple #just by copying recursive and adding a c_id loop at start

---
+++ R/omop_relations_multiple.R
@@ -0,0 +1,76 @@
+#' extract omop concept relations of a vector of concept_ids - immediate relations with indication of relationship
+#'
+#' @param mc_ids a vector of multiple concept_id's to get relations of
+#' @param c_ids one or more concept_id to filter by, default NULL for all
+#' @param d_ids one or more domain_id to filter by, default NULL for all
+#' @param v_ids one or more vocabulary_id to filter by, default NULL for all
+#' @param cc_ids one or more concept_class_id to filter by, default NULL for all
+#' @param standard one or more standard_concept to filter by, default NULL for all, S,C
+#' @param r_ids one or more relationship_id to filter by, default NULL for all, e.g c('Is a','Subsumes')
+#' @param messages whether to print info messages, default=TRUE
+#' @param num_recurse number of recursions to search
+#' @return a dataframe of concepts and attributes
+#' @export
+#' @examples
+#' #omop_relations_multiple(TODO, num_recurse=1)
+#' #omop_relations_multiple(TODO, r_ids=c('Is a','Subsumes'), num_recurse=2)
+omop_relations_multiple <- function(mc_ids,
+                                     c_ids=NULL,
+                                     d_ids=NULL,
+                                     v_ids=NULL,
+                                     cc_ids=NULL,
+                                     standard=NULL,
+                                     r_ids=NULL,
+                                     messages=TRUE,
+                                     num_recurse=1) {
+
+#DEVNOTE from NY first go at dev of multiple
+#just by copying recursive and adding a c_id loop at start
+
+
+  #checks c_id and gets name (ALL if c_id==NULL)
+  res <- check_c_id(c_id)
+  c_id <- res$c_id[1]
+  name1 <- res$name1[1]
+
+  if (messages) message("multiplely querying concept relations of: ",name1," - may take more than a few seconds")
+
+  dfall <- NULL
+
+  for(recurse in 0:num_recurse)
+  {
+
+    if (messages) message("recurse level ",recurse," of ",num_recurse)
+
+    # get relations of each concept from the previous level
+    # TODO maybe avoid or filter at end duplicate concept_id_1, concept_id_2, relationship_id
+
+    if (recurse == 0) {
+      prev_c_ids <- c_id
+    } else {
+      prev_c_ids <- unique(dfprev$concept_id_2)
+    }
+
+    dfprev <- NULL
+
+    for(c_id in prev_c_ids) {
+
+      # get immediate relations
+      dfprev1 <- omop_relations(c_id=c_id,
+                               c_ids=c_ids,
+                               d_ids=d_ids,
+                               v_ids=v_ids,
+                               cc_ids=cc_ids,
+                               standard=standard,
+                               r_ids=r_ids,
+                               messages=messages)
+
+      dfprev <- bind_rows(dfprev,dfprev1)
+    }
+
+    dfall <- bind_rows(dfall,dfprev)
+
+  }
+
+  return(dfall)
+}
