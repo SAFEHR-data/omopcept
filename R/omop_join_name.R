6e2d94b582e7a8b09f42156ee4fc81d6aff8bf73
AndySouth <southandy@gmail.com>
2024-01-10 10:54:39 +0000

    adding allcolumns option to join_name

--- R/omop_join_name.R
+++ R/omop_join_name.R
@@ -6,6 +6,7 @@
 #' @param df dataframe
 #' @param namestart start of *_concept_id column, if "" will join on concept_name, ignored if namefull used
 #' @param namefull optional full name of concept_id column, if default "" namestart used
+#' @param allcolumns whether to add all columns from omop concept table, default FALSE, if TRUE it over-rides following single column args
 #' @param domain whether to add domain_id column, default FALSE
 #' @param vocabulary whether to add vocabulary_id column, default FALSE
 #' @param concept_class whether to add concept_class_id column, default FALSE
@@ -24,6 +25,7 @@
 omop_join_name <- function(df,
                            namestart = "",
                            namefull = "",
+                           allcolumns = FALSE,
                            domain = FALSE,
                            vocabulary = FALSE,
                            concept_class = FALSE,
@@ -43,42 +45,48 @@ omop_join_name <- function(df,
   #maybe offer an option of
   #name_col_name <- sub("_concept_id","_name",id_col_name)
 
+
+  from_omop_concept <- omopcept::omop_concept()
+
+
+  if (allcolumns == FALSE )
+  {
+    columns2join <- c("concept_id","concept_name")
+
+    #optionally include other concept columns
+    #protect against problem if more than 1 id column it can become .x,.y etc.
+    if (domain == TRUE & !("domain_id" %in% names(df)))
+      columns2join <- c(columns2join, "domain_id")
+    if (vocabulary == TRUE & !("vocabulary_id" %in% names(df)))
+      columns2join <- c(columns2join, "vocabulary_id")
+    if (concept_class == TRUE & !("concept_class_id" %in% names(df)))
+      columns2join <- c(columns2join, "concept_class_id")
+    if (concept_code == TRUE & !("concept_code" %in% names(df)))
+      columns2join <- c(columns2join, "concept_code")
+    if (standard == TRUE & !("standard_concept" %in% names(df)))
+      columns2join <- c(columns2join, "standard_concept")
+    if (valid_dates == TRUE & !("valid_start_date" %in% names(df)))
+      columns2join <- c(columns2join, "valid_start_date", "valid_end_date")
+    if (invalid == TRUE & !("invalid_reason" %in% names(df)))
+      columns2join <- c(columns2join, "invalid_reason")
+
+
+    from_omop_concept <- from_omop_concept |>
+      select(any_of(columns2join))
+  }
+
   #beware rename concept_name column before joining in case
   #there is already a concept_name column in df
-
-  columns2join <- c("concept_id","concept_name")
-
-  #2023-10-05 optionally include vocabulary_id and domain_id
-  #protect against problem if more than 1 id column it can become .x,.y etc.
-  if (domain == TRUE & !("domain_id" %in% names(df)))
-    columns2join <- c(columns2join, "domain_id")
-  if (vocabulary == TRUE & !("vocabulary_id" %in% names(df)))
-    columns2join <- c(columns2join, "vocabulary_id")
-  if (concept_class == TRUE & !("concept_class_id" %in% names(df)))
-    columns2join <- c(columns2join, "concept_class_id")
-  if (concept_code == TRUE & !("concept_code" %in% names(df)))
-    columns2join <- c(columns2join, "concept_code")
-  #TODO check these
-  if (standard == TRUE & !("standard_concept" %in% names(df)))
-    columns2join <- c(columns2join, "standard_concept")
-  if (valid_dates == TRUE & !("valid_start_date" %in% names(df)))
-    columns2join <- c(columns2join, "valid_start_date", "valid_end_date")
-  if (invalid == TRUE & !("invalid_reason" %in% names(df)))
-    columns2join <- c(columns2join, "invalid_reason")
-
-  id_and_name <- omopcept::omop_concept() |>
-    select(any_of(columns2join)) |>
-    #change concept_name to the full name
+  from_omop_concept <- from_omop_concept |>
     rename_with(~name_col_name, concept_name)
 
   #TODO can I make this faster by replacing the copy=TRUE
   #with some filter & collect ?
 
   df |>
-    left_join(id_and_name, by = dynamic_by(id_col_name,"concept_id"), copy = TRUE) |>
+    left_join(from_omop_concept, by = dynamic_by(id_col_name,"concept_id"), copy = TRUE) |>
     #move name column next to id to make output more readable
-    dplyr::relocate(name_col_name, .after=id_col_name)
-
+    dplyr::relocate(name_col_name, .after = id_col_name)
 
 }
 
@@ -97,11 +105,14 @@ ojoin <- omop_join_name
 #' e.g. drug_concept_id to get drug_name etc.
 #'
 #' @param df dataframe, or a list of multiple dataframes
+#' @param allcolumns whether to add all columns from omop concept table, default FALSE, if TRUE it over-rides following single column args
 #' @param domain whether to add domain_id column, default FALSE
 #' @param vocabulary whether to add vocabulary_id column, default FALSE
 #' @param concept_class whether to add concept_class_id column, default FALSE
 #' @param concept_code whether to add concept_code column, default FALSE
-#'
+#' @param standard whether to add standard_concept column, default FALSE
+#' @param valid_dates whether to add columns valid_start_date & valid_end_date, default FALSE
+#' @param invalid whether to add invalid_reason column, default FALSE
 #' @return dataframe based on input df with 1 extra column added for each concept_id column, or a list of multiple dataframes
 #' @export
 #' @examples
@@ -120,15 +131,27 @@ ojoin <- omop_join_name
 #' #df3 <- omop_concept_relationship() |> head() |>
 #' #          dplyr::collect() |> omop_join_name_all()
 omop_join_name_all <- function(df,
+                               allcolumns = FALSE,
                                domain = FALSE,
                                vocabulary = FALSE,
                                concept_class = FALSE,
-                               concept_code = FALSE) {
+                               concept_code = FALSE,
+                               standard = FALSE,
+                               valid_dates = FALSE,
+                               invalid = FALSE) {
 
   #to apply to list of multiple tables
   #if inherits from list use lapply to call func itself on components
   if (inherits(df,'list')) {
-    alltables <- lapply(df, function(x) omop_join_name_all(x))
+    alltables <- lapply(df, function(x) omop_join_name_all(x,
+                                                           allcolumns = allcolumns,
+                                                           domain = domain,
+                                                           vocabulary = vocabulary,
+                                                           concept_class = concept_class,
+                                                           concept_code = concept_code,
+                                                           standard = standard,
+                                                           valid_dates = valid_dates,
+                                                           invalid = invalid))
     return(alltables)
   }
 
@@ -136,29 +159,10 @@ omop_join_name_all <- function(df,
   #if colname contains *_concept_id do omop_join_name(namestart=*)
   #else if colname contains concept_id do omop_join_name(namefull=colname)
 
-  #2023-01 bug
-  #data.frame(domain_concept_id_1=(c(3571338L,3655355L))) |> omop_join_name_all()
-  #Error in `left_join()` at omopcept/R/omop_join_name.R:65:2:
-  #! Join columns in `x` must be present in the data.
-  # Problem with `domain_1_concept_id`
-  #what happens
-  #concept_id gets removed from domain_concept_id_1
-  #leaving domain_1
-  #then omop_join_name(namestart=domain_1 looks for domain_1_concept_id
-  #potential solution
-  #want to use omop_join_name(namefull=
-  #so I don't want str_remove "_concept_id" to kick in
-  #can I get that str_remove to only operate
-  #if the string is at end of word ?, yes just add a $
-  #hurrah! fixes current bug, but does it cause others ?
-  #maybe this is time to add tests ?
-
-
   colnames <- df |>
     select(any_of(contains("concept_id"))) |>
     names() |>
     stringr::str_remove("_concept_id$")
-
 
   for(cname in colnames)
   {
