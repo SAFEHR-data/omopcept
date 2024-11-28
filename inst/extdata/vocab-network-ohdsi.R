f08c0381bae716c496cebed41715b7114bbd012f
Andy South <southandy@gmail.com>
2024-11-28 12:55:08 +0000

    counting inter-vocab relationships for OHDSI now working

--- inst/extdata/vocab-network-ohdsi.R
+++ inst/extdata/vocab-network-ohdsi.R
@@ -4,6 +4,8 @@
 # to make a plot requested by Patrick Ryan of relationships between vocabs
 # for the OHDSI yearbook 2025
 
+library(dplyr)
+
 # nodesize number of concepts in vocab
 # connections between vocabs that have inter-vocab connections
 # will probably need to modify omop_graph() so that it can accept
@@ -13,9 +15,10 @@
 # width of edges set by number of relationships
 # ability to filter which relationships, e.g. just 'maps to'
 
+# count number of concepts in each vocab
 vocabrships <- omop_concept_relationship() |>
                left_join(omop_concept(),by=c(concept_id_1="concept_id")) |>
-               count(vocabulary_id,sort=TRUE) |>
+               count(vocabulary_id,sort=TRUE, name="allrelationships") |>
                collect()
 
 # I could calculate in a for loop
@@ -26,21 +29,43 @@ vocabrships <- omop_concept_relationship() |>
 # vocab1 nconcepts vocab2 nrshipsbw
 # will be multiple rows of vocab1 & nconcepts will be same for each & repeated
 
-# function to count all connected vocabs for one vocab (v1)
-count_inter_vocabrships <- function (v1) {
+# function to count all connected vocabs for all vocabs
+# only takes ~10s, produces 289 rows, seems low, maybe its not doing exactly what I think ?
+# potential to filter or count a subset of relationship types
+count_inter_vocabrships <- function () {
 
   omop_concept_relationship() |>
     # join on vocab_id for concept1
-    left_join(select(omop_concept(),vocabulary_id), by=c(concept_id_1="concept_id")) |>
+    left_join(select(omop_concept(),concept_id,vocabulary_id), by=c(concept_id_1="concept_id")) |>
     rename(vocabulary_id_1=vocabulary_id) |>
-    # filter just one of concept1 (passed in arg)
-    filter(vocabulary_id_1 == v1) |>
+    # could filter just one of concept1 (passed in arg)
+    # filter(vocabulary_id_1 == v1) |>
     # join on vocab_id for concept1
-    left_join(select(omop_concept(),vocabulary_id), by=c(concept_id_2="concept_id")) |>
+    left_join(select(omop_concept(),concept_id,vocabulary_id), by=c(concept_id_2="concept_id")) |>
     rename(vocabulary_id_2=vocabulary_id) |>
 
-    count(vocabulary_id,sort=TRUE) |>
+    count(vocabulary_id_1, vocabulary_id_2, sort=FALSE, name="nrelationships") |>
+    arrange(vocabulary_id_1, desc(nrelationships)) |>
     collect()
-
 }
 
+#takes ~ 10 secs, result 289 rows
+intervocab <- count_inter_vocabrships()
+
+# cool :-)
+# vocabulary_id_1 vocabulary_id_2    nrelationships
+# 1 ABMS            ABMS                          216
+# 2 ABMS            Medicare Specialty            109
+# 3 ABMS            NUCC                           92
+# 4 ABMS            HES Specialty                  35
+# 5 ABMS            UK Biobank                     29
+# 6 ABMS            SNOMED                         25
+# 7 ATC             RxNorm Extension           165075
+# 8 ATC             RxNorm                      81703
+# 9 ATC             ATC                         13548
+# 10 ATC             SNOMED                       1794
+# ...
+
+# TODO
+# 1. get omop_graph() to cope with plotting intervocab
+# 2.
