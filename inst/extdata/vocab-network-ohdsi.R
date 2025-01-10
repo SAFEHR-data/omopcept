9aa7e10eb94c6a3442f4de259d7445c341818997
South <Andrew.South@UCLH201.NET>
2025-01-10 13:07:27 +0000

    first working vis of ohdsi vocab inter-relationships

--- inst/extdata/vocab-network-ohdsi.R
+++ inst/extdata/vocab-network-ohdsi.R
@@ -43,10 +43,16 @@ count_inter_vocabrships <- function () {
     # join on vocab_id for concept2
     left_join(select(omop_concept(),concept_id,vocabulary_id), by=c(concept_id_2="concept_id")) |>
     rename(vocabulary_id_2=vocabulary_id) |>
-
+    # DO I WANT TO FILTER OUT RSHIPS TO ITSELF
+    # (in practice they can lead to isolated dots not in network)
     count(vocabulary_id_1, vocabulary_id_2, sort=FALSE, name="nrelationships") |>
-    arrange(vocabulary_id_1, desc(nrelationships)) |>
-    collect()
+    #add the total relationships per vocab1
+    collect() |>
+    group_by(vocabulary_id_1) |>
+      mutate(total_relationships_vocab1 = sum(nrelationships)) |>
+      ungroup() |>
+    arrange(vocabulary_id_1, desc(nrelationships))
+
 }
 
 #takes ~ 10 secs, result 289 rows
@@ -66,6 +72,48 @@ intervocab <- count_inter_vocabrships()
 # 10 ATC             SNOMED                       1794
 # ...
 
+# think I want to size bubbles by the num concepts (not nrelationships)
+# so far this only uses relationships
+# query concept file & join on
+
+nconcepts_per_vocab <- omop_concept() |>
+  count(vocabulary_id, sort=TRUE, name="nconcepts") |>
+  collect()
+
+intervocab <- intervocab |>
+  left_join(nconcepts_per_vocab, by=join_by(vocabulary_id_1==vocabulary_id))
+
+# omop_graph() now copes with plotting intervocab with names vocabulary_id_1 & 2
+
+#can't colour by vocab yet because too many vocabs
+#omop_graph(intervocab, nodetxtsize = 3, nodesizevar = "nrelationships", nodecolourvar = "vocabulary_id_1")
+
+#good start but main vocabs are cramped in middle & unconnected ones around edge
+omop_graph(intervocab,
+           nodetxtsize = 5,
+           nodesizevar = "nrelationships",
+           nodesize = c(5,20),
+           legendshow=FALSE)
+#trying to colour by nrelationships, doesn't work well, more connected vocabs don't get coloured
+#omop_graph(intervocab, nodetxtsize = 5, nodesizevar = "nrelationships", nodecolourvar =  "nrelationships", legendshow=FALSE)
+
+# excluding vocabs that only link to themselves
+intervocab |>
+  #filter(nrelationships > 30) |>
+  #filter(total_relationships_vocab1 > 330 ) |>
+  filter(vocabulary_id_1 != vocabulary_id_2) |>
+  omop_graph(nodetxtsize = 7,
+             nodesizevar = "nconcepts",
+             nodesize = c(1,50),
+             nodealpha = 0.5, #default 0.8
+             #palettebrewer = "Accent",
+             edgecolour = "gold",
+             #edgecolour = "red3",
+             #palettebrewer = "Reds",
+             #palettedirection = -1, #to get strongest colour for single value
+             graphtitle = "OMOP vocabulary relationships by omopcept",
+             legendshow=FALSE)
+
 # TODO
-# 1. get omop_graph() to cope with plotting intervocab
-# 2.
+
+#
