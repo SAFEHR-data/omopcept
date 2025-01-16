6d44bb5a771072d52855499f69b6b9baa86da397
South <Andrew.South@UCLH201.NET>
2025-01-16 14:52:06 +0000

    tidyup

--- inst/extdata/vocab-network-ohdsi.R
+++ inst/extdata/vocab-network-ohdsi.R
@@ -1,6 +1,6 @@
 #vocab-network-ohdsi.R
 
-# andy south 2024-10-30
+# andy south 2025-01-16
 # to make a plot requested by Patrick Ryan of relationships between vocabs
 # for the OHDSI yearbook 2025
 
@@ -8,21 +8,18 @@ library(dplyr)
 
 # nodesize number of concepts in vocab
 # connections between vocabs that have inter-vocab connections
-# will probably need to modify omop_graph() so that it can accept
-# different column names, and use existing ones as default
 
 ## potential stretch goals
 # width of edges set by number of relationships
 # ability to filter which relationships, e.g. just 'maps to'
 
-# count number of concepts in each vocab
+# all relationships per vocab (not what I need)
 vocabrships <- omop_concept_relationship() |>
-               left_join(omop_concept(),by=c(concept_id_1="concept_id")) |>
-               count(vocabulary_id,sort=TRUE, name="allrelationships") |>
+               left_join(omop_concept(), by=c(concept_id_1="concept_id")) |>
+               count(vocabulary_id, sort=TRUE, name="allrelationships") |>
                collect()
 
-# I could calculate in a for loop
-# for (id1 in concept_id_1)
+
 
 # but still not quite sure what I want to end up with ...
 # maybe I want
@@ -30,23 +27,22 @@ vocabrships <- omop_concept_relationship() |>
 # will be multiple rows of vocab1 & nconcepts will be same for each & repeated
 
 # function to count all connected vocabs for all vocabs
-# only takes ~10s, produces 289 rows, seems low, maybe its not doing exactly what I think ?
+# ~10 secs, result ~289 rows, dependent on which vocabs downloaded from Athena
 # potential to filter or count a subset of relationship types
+# vocabulary_id_1, vocabulary_id_2, nrelationships, total_relationships_vocab1"
 count_inter_vocabrships <- function () {
 
   omop_concept_relationship() |>
     # join on vocab_id for concept1
     left_join(select(omop_concept(),concept_id,vocabulary_id), by=c(concept_id_1="concept_id")) |>
     rename(vocabulary_id_1=vocabulary_id) |>
-    # could filter just one of concept1 (passed in arg)
-    # filter(vocabulary_id_1 == v1) |>
+    # could filter one or more of concept1 (passed in optional arg)
+    # filter(vocabulary_id_1 %in% v1) |>
     # join on vocab_id for concept2
     left_join(select(omop_concept(),concept_id,vocabulary_id), by=c(concept_id_2="concept_id")) |>
     rename(vocabulary_id_2=vocabulary_id) |>
-    # DO I WANT TO FILTER OUT RSHIPS TO ITSELF
-    # (in practice they can lead to isolated dots not in network)
     count(vocabulary_id_1, vocabulary_id_2, sort=FALSE, name="nrelationships") |>
-    #add the total relationships per vocab1
+    #add the total relationships per vocab1 (although may not be needed)
     collect() |>
     group_by(vocabulary_id_1) |>
       mutate(total_relationships_vocab1 = sum(nrelationships)) |>
@@ -55,7 +51,7 @@ count_inter_vocabrships <- function () {
 
 }
 
-#takes ~ 10 secs, result 289 rows
+#takes ~10 secs, result ~289 rows, dependent on which vocabs downloaded from Athena
 intervocab <- count_inter_vocabrships()
 
 # cool :-)
@@ -72,10 +68,7 @@ intervocab <- count_inter_vocabrships()
 # 10 ATC             SNOMED                       1794
 # ...
 
-# think I want to size bubbles by the num concepts (not nrelationships)
-# so far this only uses relationships
-# query concept file & join on
-
+# to be able to size bubbles by num concepts (not nrelationships)
 nconcepts_per_vocab <- omop_concept() |>
   count(vocabulary_id, sort=TRUE, name="nconcepts") |>
   collect()
@@ -84,20 +77,21 @@ intervocab <- intervocab |>
   left_join(nconcepts_per_vocab, by=join_by(vocabulary_id_1==vocabulary_id))
 
 # omop_graph() now copes with plotting intervocab with names vocabulary_id_1 & 2
+# can't colour by vocab yet because too many vocabs (I need to improve palette options)
+# omop_graph(intervocab, nodetxtsize = 3, nodesizevar = "nrelationships", nodecolourvar = "vocabulary_id_1")
 
-#can't colour by vocab yet because too many vocabs
-#omop_graph(intervocab, nodetxtsize = 3, nodesizevar = "nrelationships", nodecolourvar = "vocabulary_id_1")
-
-#good start but main vocabs are cramped in middle & unconnected ones around edge
+# good start but main vocabs are cramped in middle & unconnected ones around edge
 omop_graph(intervocab,
            nodetxtsize = 5,
            nodesizevar = "nrelationships",
            nodesize = c(5,20),
            legendshow=FALSE)
-#trying to colour by nrelationships, doesn't work well, more connected vocabs don't get coloured
-#omop_graph(intervocab, nodetxtsize = 5, nodesizevar = "nrelationships", nodecolourvar =  "nrelationships", legendshow=FALSE)
+
+# trying to colour by nrelationships, doesn't work well, more connected vocabs don't get coloured
+# omop_graph(intervocab, nodetxtsize = 5, nodesizevar = "nrelationships", nodecolourvar =  "nrelationships", legendshow=FALSE)
 
 # excluding vocabs that only link to themselves
+# in practice they seem to lead a cramped network in centre with isolated dots on edge
 intervocab |>
   #filter(nrelationships > 30) |>
   #filter(total_relationships_vocab1 > 330 ) |>
@@ -105,15 +99,14 @@ intervocab |>
   omop_graph(nodetxtsize = 7,
              nodesizevar = "nconcepts",
              nodesize = c(1,50),
-             nodealpha = 0.5, #default 0.8
-             #palettebrewer = "Accent",
+             nodealpha = 0.7, #default 0.8
+             #default palette Dark2 looks better, try later to get blue of OHDSI logo :-)
+             #palettebrewer = "PRGn", #"RdBu",
+             #palettedirection = -1, #fails to get strongest colour for single value
              edgecolour = "gold",
-             #edgecolour = "red3",
-             #palettebrewer = "Reds",
-             #palettedirection = -1, #to get strongest colour for single value
              graphtitle = "OMOP vocabulary relationships by omopcept",
              legendshow=FALSE)
 
 # TODO
-
-#
+# make colour palette more flexible from omop_graph() e.g. allow single colour choice
+# look into edge width setting
