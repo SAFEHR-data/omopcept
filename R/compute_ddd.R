bfbeaeb17371a3bf388070627fd4b7e4fc387150
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-02-10 21:13:16 +0000

    Fix the bug in the compute_ddd function This calcualtion works fine now but the code is not very efficient. I will need to check this later.

--- R/compute_ddd.R
+++ R/compute_ddd.R
@@ -68,18 +68,37 @@ compute_ddd <- function(drug_code = NULL,
     filtered_drug_exposure <- filtered_drug_exposure |>
         dplyr::left_join(atc_ddd_table, by = c("ATC_code" = "atc_code", "atc_route" = "adm_r"))
 
-    # TODO: Implement DDD calculation
-    # Possible approach using units package:
+    # TODO: improve this calculation
+    # This works now, but it is so slow because the units package is slow
+    # and it is called for each row. I think if i do some sort of grouping
+    # this might work faster
     filtered_drug_exposure <- filtered_drug_exposure |>
         dplyr::mutate(
-            ddd = units::set_units(quantity * combined_value, combined_unit) /
-                units::set_units(ddd, uom_as_units)
-        )
+            ddd_per_exposure = vapply(seq_len(nrow(filtered_drug_exposure)), function(i) {
+                qty <- as.numeric(filtered_drug_exposure$quantity[i])
+                cval <- as.numeric(filtered_drug_exposure$combined_value[i])
+                ddd_val <- as.numeric(filtered_drug_exposure$ddd[i])
 
-    # RAMSES uses mixed units for DDD
-    # I am not sure how to do the calculation correctly
-    # the code that RAMSES uses is:
-    # DDD := as.numeric(
-    #  units::mixed_units(x = dose, value = unit) /
-    #    units::mixed_units(x = ddd_value, value = u)
+                if (is.na(qty) || is.na(cval) || is.na(ddd_val)) {
+                    return(NA_real_)
+                }
+
+                tryCatch(
+                    {
+                        num <- units::set_units(
+                            x = (qty * cval),
+                            value = filtered_drug_exposure$combined_unit[i],
+                            mode = "standard"
+                        )
+                        den <- units::set_units(
+                            x = ddd_val,
+                            value = filtered_drug_exposure$uom[i],
+                            mode = "standard"
+                        )
+                        as.numeric(num / den)
+                    },
+                    error = function(e) NA_real_
+                )
+            }, FUN.VALUE = numeric(1))
+        )
 }
