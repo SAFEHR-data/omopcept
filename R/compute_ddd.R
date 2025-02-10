636d7f1920c2f8ee83cfc40168e76e964e5be145
Mahmoud Abdelrazek <razekmh@gmail.com>
2025-02-10 19:40:51 +0000

    remove ATC option of DDD input

--- R/compute_ddd.R
+++ R/compute_ddd.R
@@ -9,15 +9,10 @@
 #' @return Data frame with DDD calculations per drug exposure
 #' @export
 
-compute_ddd <- function(mode = "atc",
-                        drug_code = NULL,
+compute_ddd <- function(drug_code = NULL,
                         drug_exposure_table = NULL,
                         atc_ddd_path = NULL) {
     # Input validation
-    if (!mode %in% c("atc", "omop")) {
-        stop("Mode must be either 'atc' or 'omop'")
-    }
-
     if (!is.null(drug_code)) {
         stop("Drug code must be NULL")
     }
@@ -25,7 +20,6 @@ compute_ddd <- function(mode = "atc",
     if (is.null(drug_exposure_table)) {
         stop("Drug exposure table must be provided")
     }
-
     # Check if drug_code is a string and convert to list if needed
     if (is.character(drug_code) && length(drug_code) == 1) {
         drug_code <- list(drug_code)
@@ -33,46 +27,9 @@ compute_ddd <- function(mode = "atc",
         stop("drug_code must be either a string or a list")
     }
 
-    if (mode == "atc") {
-        # create drug_lookup table
-        drug_lookup <- omop_drug_lookup_create(drug_exposure_table, drug_concept_vocabs = c("RxNorm", "RxNorm Extension"))
-
-        # get the OMOP concept_id for the drug code
-        filtered_drug_lookup <- drug_lookup |>
-            dplyr::filter(ATC_code %in% drug_code)
-
-        # Check for duplicate drug_concept_ids
-        duplicate_concepts <- filtered_drug_lookup |>
-            dplyr::group_by(drug_concept_id) |>
-            dplyr::filter(dplyr::n() > 1)
-
-        if (nrow(duplicate_concepts) > 0) {
-            # Group by drug_concept_id and concatenate ATC codes
-            warning_msg <- duplicate_concepts |>
-                dplyr::group_by(drug_concept_id) |>
-                dplyr::summarise(atc_codes = paste(ATC_code, collapse = ", ")) |>
-                dplyr::mutate(msg = sprintf(
-                    "drug_concept_id %d appears for ATC codes: %s",
-                    drug_concept_id, atc_codes
-                )) |>
-                dplyr::pull(msg) |>
-                paste(collapse = "\n")
-
-            warning(
-                "Duplicate drug_concept_ids found for ATC codes
-                This may cause issues in the DDD computation:\n",
-                warning_msg
-            )
-        }
-        filtered_drug_lookup <- filtered_drug_lookup |>
-            dplyr::distinct(drug_concept_id)
-    } else if (mode == "omop") {
-        filtered_drug_lookup <- drug_code
-    }
-
     # filter drug_exposure_table for the drug_concept_ids
     filtered_drug_exposure <- drug_exposure_table |>
-        dplyr::filter(drug_concept_id %in% filtered_drug_lookup$drug_concept_id)
+        dplyr::filter(drug_concept_id %in% drug_code)
 
     # get the route of administration
     filtered_drug_exposure <- filtered_drug_exposure |>
